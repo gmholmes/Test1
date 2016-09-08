0d8252cf3994223a6bfa9ab6a2205f13c1b99aab
Gary Holmes <gary@heathdale.com>
2016-09-08 17:30:01 +0100

    Bigger button

--- Test1/Form1.Designer.cs
+++ Test1/Form1.Designer.cs
@@ -34,9 +34,10 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(109, 53);
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(97, 53);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(93, 58);
             this.button1.TabIndex = 0;
             this.button1.Text = "Press Me!";
             this.button1.UseVisualStyleBackColor = true;
